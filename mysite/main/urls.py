0798dac5a3179d0aa70177f8afa0d733e3de7621
Konstantinos Biris <sc19kb@leeds.ac.uk>
2022-03-04 14:28:38 +0000

    Make Register User Functionality

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    path("register/", views.register, name="register"),
     path("", views.index, name="index"),
-    path("home/", views.home, name="home")
+    path("home/", views.home, name="home"),
+    
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
-from .models import *
+from django.shortcuts import redirect, render
 
+from .forms import NewUserForm
+from .models import *
 
 # Create your views here.
 
@@ -10,3 +13,14 @@ def index(response):
 
 def home(response):
     return render(response, "main/home.html", {})    
+
+def register(response):
+	if response.method == "POST":
+		form = UserCreationForm(response.POST)
+		if form.is_valid():
+			form.save()
+			return redirect("/home")
+	else:
+		form = UserCreationForm()
+		
+	return render(response, "main/register.html", {"form":form})
